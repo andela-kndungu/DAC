41054b473fdb7fc22587764b953f6c16fbac212a
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2017-01-02 11:02:54 +0300

    Success!

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+
         int color = ResourcesCompat.getColor(getResources(), R.color.colorAccent, null);
         view.setBackgroundColor(color);
 
+        startActivity(intent);
     }
 }
