d27d13d37498a80d69d5669f5fee9d7310db090a
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2017-01-02 10:19:21 +0300

    Change button colour on click

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        int color = ResourcesCompat.getColor(getResources(), R.color.colorAccent, null);
+        view.setBackgroundColor(color);
+
     }
 }
